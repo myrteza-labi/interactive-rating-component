56465256435b6f6710bf1c698168a64145c0a566
myrteza <myrteza@hotmail.fr>
2023-08-02 16:38:58 +0200

    fonctionnal frontend done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useForm } from "react-hook-form";
 import React, { useState } from 'react';
-import { map } from 'ramda';
+import { map, isEmpty } from 'ramda';
 
 const ratingValue = ["1", "2", "3", "4", "5"];
 
@@ -9,32 +9,44 @@ const App = () => {
   const [data, setData] = useState([]);
 
   const onSubmit = (formData) => {
-    setData(JSON.stringify(formData));
+    setData(formData);
   };
 
-  return (
-    <div className="">
-      <div className=""></div>
-      <h1>How did we do?</h1>
-      <p>
-        Please let us know how we did with your support request.
-        All feedback is appreciated to help us improve our offering!
-      </p>
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {map((value) =>
-          <div key={value}>
-            <label>{value}</label>
-            <input type="radio" {...register("rating")} value={value} name="rating" />
-          </div>
-          , ratingValue
-        )}
-        <input type="submit" />
-      </form>
-      <div>
-        {data}
-      </div>
+  const successUi = <>
+    <img src="" alt=""/>
+    <div>
+      <p>You selected {data.rating} out of {ratingValue.length}</p>
     </div>
-  )
+    <h1>Thank you!</h1>
+    <p>
+      We appreciate you taking the time to give a rating. 
+      If you ever need more support, don’t hesitate to get in touch!
+    </p>
+  </>
+
+  const ratingUi = <div className="">
+  <div className=""></div>
+  <h1>How did we do?</h1>
+  <p>
+    Please let us know how we did with your support request.
+    All feedback is appreciated to help us improve our offering!
+  </p>
+  <form onSubmit={handleSubmit(onSubmit)}>
+    {map((value) =>
+      <div key={value}>
+        <label>{value}</label>
+        <input type="radio" {...register("rating")} value={value} name="rating" />
+      </div>
+      , ratingValue
+    )}
+    <input type="submit" />
+  </form>
+  <div>
+  </div>
+</div>
+
+  return isEmpty(data) ? ratingUi : successUi; 
+  
 }
 
 export default App;
